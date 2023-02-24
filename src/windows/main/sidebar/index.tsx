7930d2e74464c4968c745d027b00548c873c14f4
jacob <jacobgluszek03@gmail.com>
2023-02-24 12:13:01 +0100

    patch: sidebar refactored & added open/close animation

--- src/windows/main/sidebar/index.tsx
+++ src/windows/main/sidebar/index.tsx
@@ -8,6 +8,7 @@ import {
   MdStickyNote2,
 } from "react-icons/md";
 import { WebviewWindow } from "@tauri-apps/api/window";
+import { AnimatePresence, motion } from "framer-motion";
 
 import useStore from "@/store";
 import config from "@/config";
@@ -26,120 +27,131 @@ type Tab = "intents" | "notes" | "tasks";
 const Sidebar: React.FC<Props> = (props) => {
   const [tab, setTab] = React.useState<Tab>("intents");
 
-  const activeIntentId = useStore((state) => state.activeIntentId);
-
-  React.useEffect(() => {
-    if (!activeIntentId) setTab("intents");
-  }, [activeIntentId]);
-
-  return (
-    <div
-      className="z-[9999] left-0 top-0 fixed w-[286px] h-full flex flex-col bg-window transition-opacity"
-      style={{
-        boxShadow: "8px 16px 24px -8px rgba(0, 0, 0, 0.60)",
-        zIndex: props.isVisible ? 9999 : -1,
-        opacity: props.isVisible ? 1.0 : 0.0,
-      }}
-    >
-      <div className="grow flex flex-col p-2 bg-darker/40">
-        <Header onCollapse={() => props.collapse()} />
-        <div className="grow flex flex-col py-1">
-          {tab === "intents" ? <IntentsView /> : null}
-          {tab === "tasks" ? <TasksView /> : null}
-          {tab === "notes" ? <NotesView /> : null}
-        </div>
-        {activeIntentId ? (
-          <Tabs value={tab} onChange={(value) => setTab(value)} />
-        ) : null}
-      </div>
-    </div>
-  );
-};
-
-interface HeaderProps {
-  onCollapse: () => void;
-}
-
-const Header: React.FC<HeaderProps> = (props) => {
-  return (
-    <div className="w-full flex flex-row items-center justify-between">
-      <div className="flex flex-row items-center gap-2">
-        <Button transparent onClick={props.onCollapse}>
-          <TbLayoutSidebarLeftCollapse size={28} />
-        </Button>
-        <Button
-          transparent
-          onClick={() =>
-            new WebviewWindow("settings", config.webviews.settings)
-          }
-        >
-          <MdSettings size={28} />
-        </Button>
-      </div>
-      <Button
-        transparent
-        onClick={() => new WebviewWindow("intents", config.webviews.intents)}
-      >
-        <MdOpenInNew size={28} />
-      </Button>
-    </div>
-  );
-};
-
-interface TabsProps {
-  value: Tab;
-  onChange: (value: Tab) => void;
-}
-
-const Tabs: React.FC<TabsProps> = (props) => {
   const store = useStore();
 
+  React.useEffect(() => {
+    if (!store.activeIntentId) setTab("intents");
+  }, [store.activeIntentId]);
+
   return (
-    <div className="h-7 flex flex-row gap-0.5 rounded-sm overflow-clip animate-in slide-in-from-bottom-8 zoom-in-75">
-      <Button
-        className="transition-none"
-        rounded={false}
-        style={{
-          width: props.value === "intents" ? "100%" : "fit-content",
-          paddingInline: 4,
-        }}
-        isSelected={props.value === "intents"}
-        onClick={() => props.onChange("intents")}
-      >
-        <BiTargetLock size={24} />
-        {props.value === "intents" ? <span>Intents</span> : undefined}
-      </Button>
-      {store.activeIntentId ? (
-        <Button
-          className="transition-none"
-          rounded={false}
+    <AnimatePresence>
+      {props.isVisible && (
+        <motion.div
+          className="z-[9999] left-0 top-0 fixed h-full flex flex-col bg-window transition-opacity"
           style={{
-            width: props.value === "tasks" ? "100%" : "fit-content",
-            paddingInline: 4,
+            boxShadow: "8px 16px 24px -8px rgba(0, 0, 0, 0.60)",
+            zIndex: props.isVisible ? 9999 : -1,
+            opacity: props.isVisible ? 1.0 : 0.0,
           }}
-          isSelected={props.value === "tasks"}
-          onClick={() => props.onChange("tasks")}
-        >
-          <MdCheckBox size={24} />
-          {props.value === "tasks" ? <span>Tasks</span> : undefined}
-        </Button>
-      ) : null}
-      {store.activeIntentId ? (
-        <Button
-          className="transition-none"
-          rounded={false}
-          style={{
-            width: props.value === "notes" ? "100%" : "fit-content",
-            paddingInline: 4,
+          initial={{ width: 0 }}
+          animate={{
+            width: 300,
+            transition: { duration: 0.3 },
           }}
-          isSelected={props.value === "notes"}
-          onClick={() => props.onChange("notes")}
+          exit={{
+            width: 0,
+            translateX: -32,
+            transition: { duration: 0.3, delay: 0.05 },
+          }}
         >
-          <MdStickyNote2 size={24} />
-          {props.value === "notes" ? <span>Notes</span> : undefined}
-        </Button>
-      ) : null}
-    </div>
+          <div className="grow flex flex-col p-2 bg-darker/40">
+            <motion.div
+              className="grow flex flex-col"
+              initial={{ opacity: 0 }}
+              animate={{
+                transition: { duration: 0.2, delay: 0.1 },
+                opacity: 1,
+              }}
+              exit={{
+                opacity: 0.0,
+                transition: { duration: 0.2 },
+              }}
+            >
+              {/* Header */}
+              <div className="w-full flex flex-row items-center justify-between">
+                <div className="flex flex-row items-center gap-2">
+                  <Button transparent onClick={() => props.collapse()}>
+                    <TbLayoutSidebarLeftCollapse size={28} />
+                  </Button>
+                  <Button
+                    transparent
+                    onClick={() =>
+                      new WebviewWindow("settings", config.webviews.settings)
+                    }
+                  >
+                    <MdSettings size={28} />
+                  </Button>
+                </div>
+                <Button
+                  transparent
+                  onClick={() =>
+                    new WebviewWindow("intents", config.webviews.intents)
+                  }
+                >
+                  <MdOpenInNew size={28} />
+                </Button>
+              </div>
+
+              {/* Content */}
+              <div className="grow flex flex-col py-1">
+                {tab === "intents" ? <IntentsView /> : null}
+                {tab === "tasks" ? <TasksView /> : null}
+                {tab === "notes" ? <NotesView /> : null}
+              </div>
+
+              {/* Tabs */}
+              {store.activeIntentId ? (
+                <div className="h-7 flex flex-row gap-0.5 rounded-sm overflow-clip animate-in slide-in-from-bottom-8 zoom-in-75">
+                  <Button
+                    className="transition-none"
+                    rounded={false}
+                    style={{
+                      width: tab === "intents" ? "100%" : "fit-content",
+                      paddingInline: 4,
+                    }}
+                    isSelected={tab === "intents"}
+                    onClick={() => setTab("intents")}
+                  >
+                    <BiTargetLock size={24} />
+                    {tab === "intents" ? <span>Intents</span> : undefined}
+                  </Button>
+                  {store.activeIntentId ? (
+                    <Button
+                      className="transition-none"
+                      rounded={false}
+                      style={{
+                        width: tab === "tasks" ? "100%" : "fit-content",
+                        paddingInline: 4,
+                      }}
+                      isSelected={tab === "tasks"}
+                      onClick={() => setTab("tasks")}
+                    >
+                      <MdCheckBox size={24} />
+                      {tab === "tasks" ? <span>Tasks</span> : undefined}
+                    </Button>
+                  ) : null}
+                  {store.activeIntentId ? (
+                    <Button
+                      className="transition-none"
+                      rounded={false}
+                      style={{
+                        width: tab === "notes" ? "100%" : "fit-content",
+                        paddingInline: 4,
+                      }}
+                      isSelected={tab === "notes"}
+                      onClick={() => setTab("notes")}
+                    >
+                      <MdStickyNote2 size={24} />
+                      {tab === "notes" ? <span>Notes</span> : undefined}
+                    </Button>
+                  ) : null}
+                </div>
+              ) : null}
+            </motion.div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
