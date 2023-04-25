3be4d176ac791aa059d65f58246914957a03dacf
jacob <jacobgluszek03@gmail.com>
2023-04-25 12:47:15 +0200

    Default hotkeys info

--- src/windows/settings/GeneralView.tsx
+++ src/windows/settings/GeneralView.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 
-import { Pane } from "@/ui";
+import { Card, Pane } from "@/ui";
 
 const GeneralView: React.FC = () => {
   return (
     <Pane className="grow flex flex-col">
-      <div>Hotkeys</div>
+      <HotkeysSection />
     </Pane>
+  );
+};
+
+const HotkeysSection: React.FC = () => {
+  return (
+    <div className="flex flex-col gap-1">
+      <h2>Hotkeys</h2>
+      <Card className="flex flex-row items-center justify-between">
+        <div>
+          <span className="text-primary/80 font-semibold">Timer</span> -
+          Start/Resume
+        </div>
+        <div className="w-12 p-1 bg-primary/20 text-center rounded-sm shadow-inner shadow-black/20">
+          F1
+        </div>
+      </Card>
+      <Card className="flex flex-row items-center justify-between">
+        <div>
+          <span className="text-primary/80 font-semibold">Timer</span> - Skip
+        </div>
+        <div className="w-12 p-1 bg-primary/20 text-center rounded-sm shadow-inner shadow-black/20">
+          F2
+        </div>
+      </Card>
+    </div>
   );
 };
 
