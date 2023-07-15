b66a4b4343b873ac941213e8d95719f6f592477b
jacob <jacobgluszek03@gmail.com>
2023-07-15 13:34:58 +0200

    Moved window container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import { WindowContainer } from "@/components";
-
 import { IntentsView } from "./windows/main/intents";
 import { MainWindow } from "./windows/main";
 import { TimerView } from "./windows/main/timer";
@@ -11,16 +9,14 @@ import AnalyticsWindow from "./windows/analytics";
 
 const App: React.FC = () => {
   return (
-    <WindowContainer>
-      <Routes>
-        <Route path="*" element={<MainWindow />}>
-          <Route path="intents" element={<IntentsView />} />
-          <Route path="timer" element={<TimerView />} />
-        </Route>
-        <Route path="/settings" element={<SettingsWindow />} />
-        <Route path="/analytics" element={<AnalyticsWindow />} />
-      </Routes>
-    </WindowContainer>
+    <Routes>
+      <Route path="*" element={<MainWindow />}>
+        <Route path="intents" element={<IntentsView />} />
+        <Route path="timer" element={<TimerView />} />
+      </Route>
+      <Route path="/settings" element={<SettingsWindow />} />
+      <Route path="/analytics" element={<AnalyticsWindow />} />
+    </Routes>
   );
 };
 
