6aa54f95dca15eef1d0b84ffb0ae2db9e912213a
jacob <jacobgluszek03@gmail.com>
2023-07-23 18:12:06 +0200

    Deleted legacy code

--- legacy/main/ConfigView.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { PanelView, PanelViewProps } from "./PanelView";
-
-export const ConfigView: React.FC<PanelViewProps> = (props) => {
-  return (
-    <PanelView {...props}>Edit label, Edit tags, Archive, Delete</PanelView>
-  );
-};
